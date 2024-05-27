350cbd11b29e6317f4b6d8fef201d4b1ece9c491
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-27 10:29:07 -1200

    Update header.h

--- headers/header.h
+++ headers/header.h
@@ -1,4 +1,3 @@
-
 #ifndef HEADER_H
 #define HEADER_H
 
@@ -10,31 +9,20 @@
 #include <stdbool.h>
 #include "upng.h"
 
-
 /* Constants */
-
 #define PI 3.14159265
 #define TWO_PI 6.28318530
-
 #define TILE_SIZE 64
-
 #define MINIMAP_SCALE_FACTOR 0.25
-
 #define SCREEN_WIDTH (MAP_NUM_COLS * TILE_SIZE)
 #define SCREEN_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
-
 #define FOV_ANGLE (60 * (PI / 180))
-
 #define NUM_RAYS SCREEN_WIDTH
-
 #define PROJ_PLANE ((SCREEN_WIDTH / 2) / tan(FOV_ANGLE / 2))
-
 #define FPS 30
 #define FRAME_TIME_LENGTH (1000 / FPS)
-
 #define MAP_NUM_ROWS 13
 #define MAP_NUM_COLS 20
-
 #define NUM_TEXTURES 8
 
 typedef uint32_t color_t;
@@ -44,7 +32,6 @@ void handleInput(void);
 extern bool GameRunning;
 
 /* Functions-variables-structs for draw */
-
 bool initializeWindow(void);
 void destroyWindow(void);
 void clearColorBuffer(color_t color);
@@ -61,31 +48,17 @@ void renderMap(void);
 int getMapValue(int row, int col);
 
 /* Functions-variables-structs for player */
-
-/**
- * struct player_s - struct for the textures
- * @x: x coordinate
- * @y: y coordinate
- * @width: player width
- * @height: player height
- * @turnDirection: Turn Direction
- * @walkDirection: Walk Direction
- * @rotationAngle: player rotation angle
- * @walkSpeed: walk speed
- * @turnSpeed: turn speed
- */
-
 typedef struct player_s
 {
-	float x;
-	float y;
-	float width;
-	float height;
-	int turnDirection;
-	int walkDirection;
-	float rotationAngle;
-	float walkSpeed;
-	float turnSpeed;
+    float x;
+    float y;
+    float width;
+    float height;
+    int turnDirection;
+    int walkDirection;
+    float rotationAngle;
+    float walkSpeed;
+    float turnSpeed;
 } player_t;
 
 extern player_t player;
@@ -94,25 +67,14 @@ void movePlayer(float DeltaTime);
 void renderPlayer(void);
 
 /* Functions-variables-structs for ray */
-
-/**
- * struct ray_s - struct for the textures
- * @rayAngle: ray angle
- * @wallHitX: wall hit x coordinate
- * @wallHitY: wall hit x coordinate
- * @distance: ditance to the wall
- * @wasHitVertical: verify hit vertical
- * @wallHitContent: wall hit content
- */
-
 typedef struct ray_s
 {
-	float rayAngle;
-	float wallHitX;
-	float wallHitY;
-	float distance;
-	bool wasHitVertical;
-	int wallHitContent;
+    float rayAngle;
+    float wallHitX;
+    float wallHitY;
+    float distance;
+    bool wasHitVertical;
+    int wallHitContent;
 } ray_t;
 
 extern ray_t rays[NUM_RAYS];
@@ -129,32 +91,20 @@ void horzIntersection(float rayAngle);
 void vertIntersection(float rayAngle);
 
 /* Functions-variables-structs for textures */
-
-/**
- * struct texture_s - struct for the textures
- * @width: texture width
- * @height: texture height
- * @texture_buffer: pointer to texture buffer
- * @upngTexture: pointer to upng buffer
- *
- */
-
 typedef struct texture_s
 {
-	int width;
-	int height;
-	color_t *texture_buffer;
-	upng_t *upngTexture;
+    int width;
+    int height;
+    color_t *texture_buffer;
+    upng_t *upngTexture;
 } texture_t;
 
-texture_t wallTextures[NUM_TEXTURES];
+extern texture_t wallTextures[NUM_TEXTURES];
 
 void WallTexturesready(void);
 void freeWallTextures(void);
 
 /* Functions-variables-structs for walls */
-
 void renderWall(void);
 
-#endif /*HEADER_H*/
-
+#endif /* HEADER_H */
