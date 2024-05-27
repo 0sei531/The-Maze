c7f4477eeccf78bfa67c1a256f360c472e2b5744
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-27 10:14:44 -1200

    Update upng.h

--- headers/upng.h
+++ headers/upng.h
@@ -1,160 +1,82 @@
-
-#ifndef HEADER_H
-#define HEADER_H
-
-#include <SDL2/SDL.h>
-#include <stdint.h>
-#include <math.h>
-#include <limits.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include "upng.h"
-
-
-/* Constants */
-
-#define PI 3.14159265
-#define TWO_PI 6.28318530
-
-#define TILE_SIZE 64
-
-#define MINIMAP_SCALE_FACTOR 0.25
-
-#define SCREEN_WIDTH (MAP_NUM_COLS * TILE_SIZE)
-#define SCREEN_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
-
-#define FOV_ANGLE (60 * (PI / 180))
-
-#define NUM_RAYS SCREEN_WIDTH
-
-#define PROJ_PLANE ((SCREEN_WIDTH / 2) / tan(FOV_ANGLE / 2))
-
-#define FPS 30
-#define FRAME_TIME_LENGTH (1000 / FPS)
-
-#define MAP_NUM_ROWS 13
-#define MAP_NUM_COLS 20
-
-#define NUM_TEXTURES 8
-
-typedef uint32_t color_t;
-
-/* Process Input */
-void handleInput(void);
-extern bool GameRunning;
-
-/* Functions-variables-structs for draw */
-
-bool initializeWindow(void);
-void destroyWindow(void);
-void clearColorBuffer(color_t color);
-void render_game(void);
-void renderColorBuffer(void);
-void drawPixel(int x, int y, color_t color);
-void drawRect(int x, int y, int width, int height, color_t color);
-void drawLine(int x0, int y0, int x1, int y1, color_t color);
-
-/* Functions-variables-structs for map */
-bool DetectCollision(float x, float y);
-bool isInsideMap(float x, float y);
-void renderMap(void);
-int getMapValue(int row, int col);
-
-/* Functions-variables-structs for player */
-
+#ifndef UPNG_H
+#define UPNG_H
 /**
- * struct player_s - struct for the textures
- * @x: x coordinate
- * @y: y coordinate
- * @width: player width
- * @height: player height
- * @turnDirection: Turn Direction
- * @walkDirection: Walk Direction
- * @rotationAngle: player rotation angle
- * @walkSpeed: walk speed
- * @turnSpeed: turn speed
- */
+* uPNG -- derived from LodePNG version 20100808
+*
+* Copyright (c) 2005-2010 Lode Vandevenne
+* Copyright (c) 2010 Sean Middleditch
+*
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+*
+*		1. The origin of this software must not be misrepresented; you must not
+*		claim that you wrote the original software. If you use this software
+*		in a product, an acknowledgment in the product documentation would be
+*		appreciated but is not required.
+*
+*		2. Altered source versions must be plainly marked as such, and must not be
+*		misrepresented as being the original software.
+*
+*		3. This notice may not be removed or altered from any source
+*		distribution.
+*/
 
-typedef struct player_s
+typedef enum upng_error
 {
-	float x;
-	float y;
-	float width;
-	float height;
-	int turnDirection;
-	int walkDirection;
-	float rotationAngle;
-	float walkSpeed;
-	float turnSpeed;
-} player_t;
+	UPNG_EOK			= 0, /* success (no error) */
+	UPNG_ENOMEM			= 1, /* memory allocation failed */
+	UPNG_ENOTFOUND		= 2, /* resource not found (file missing) */
+	UPNG_ENOTPNG		= 3, /* image data does not have a PNG header */
+	UPNG_EMALFORMED		= 4, /* image data is not a valid PNG image */
+	UPNG_EUNSUPPORTED	= 5, /* critical PNG chunk type is not supported */
+	UPNG_EUNINTERLACED	= 6, /* image interlacing is not supported */
+	UPNG_EUNFORMAT		= 7, /* image color format is not supported */
+	UPNG_EPARAM			= 8  /* invalid parameter to method call */
+} upng_error;
 
-extern player_t player;
-
-void movePlayer(float DeltaTime);
-void renderPlayer(void);
-
-/* Functions-variables-structs for ray */
-
-/**
- * struct ray_s - struct for the textures
- * @rayAngle: ray angle
- * @wallHitX: wall hit x coordinate
- * @wallHitY: wall hit x coordinate
- * @distance: ditance to the wall
- * @wasHitVertical: verify hit vertical
- * @wallHitContent: wall hit content
- */
-
-typedef struct ray_s
+typedef enum upng_format
 {
-	float rayAngle;
-	float wallHitX;
-	float wallHitY;
-	float distance;
-	bool wasHitVertical;
-	int wallHitContent;
-} ray_t;
+	UPNG_BADFORMAT,
+	UPNG_RGB8,
+	UPNG_RGB16,
+	UPNG_RGBA8,
+	UPNG_RGBA16,
+	UPNG_LUMINANCE1,
+	UPNG_LUMINANCE2,
+	UPNG_LUMINANCE4,
+	UPNG_LUMINANCE8,
+	UPNG_LUMINANCE_ALPHA1,
+	UPNG_LUMINANCE_ALPHA2,
+	UPNG_LUMINANCE_ALPHA4,
+	UPNG_LUMINANCE_ALPHA8
+} upng_format;
 
-extern ray_t rays[NUM_RAYS];
+typedef struct upng_t upng_t;
 
-float distanceBetweenPoints(float x1, float y1, float x2, float y2);
-bool isRayFacingUp(float angle);
-bool isRayFacingDown(float angle);
-bool isRayFacingLeft(float angle);
-bool isRayFacingRight(float angle);
-void castAllRays(void);
-void castRay(float rayAngle, int stripId);
-void renderRays(void);
-void horzIntersection(float rayAngle);
-void vertIntersection(float rayAngle);
+upng_t *upng_new_from_bytes(const unsigned char *buffer, unsigned long size);
+upng_t *upng_new_from_file(const char *path);
+void upng_free(upng_t *upng);
 
-/* Functions-variables-structs for textures */
+upng_error upng_header(upng_t *upng);
+upng_error upng_decode(upng_t *upng);
 
-/**
- * struct texture_s - struct for the textures
- * @width: texture width
- * @height: texture height
- * @texture_buffer: pointer to texture buffer
- * @upngTexture: pointer to upng buffer
- *
- */
+upng_error upng_get_error(const upng_t *upng);
+unsigned int upng_get_error_line(const upng_t *upng);
 
-typedef struct texture_s
-{
-	int width;
-	int height;
-	color_t *texture_buffer;
-	upng_t *upngTexture;
-} texture_t;
+unsigned int upng_get_width(const upng_t *upng);
+unsigned int upng_get_height(const upng_t *upng);
+unsigned int upng_get_bpp(const upng_t *upng);
+unsigned int upng_get_bitdepth(const upng_t *upng);
+unsigned int upng_get_components(const upng_t *upng);
+unsigned int upng_get_pixelsize(const upng_t *upng);
+upng_format	upng_get_format(const upng_t *upng);
 
-texture_t wallTextures[NUM_TEXTURES];
+const unsigned char *upng_get_buffer(const upng_t *upng);
+unsigned int upng_get_size(const upng_t *upng);
 
-void WallTexturesready(void);
-void freeWallTextures(void);
-
-/* Functions-variables-structs for walls */
-
-void renderWall(void);
-
-#endif /*HEADER_H*/
-
+#endif /*defined(UPNG_H)*/
